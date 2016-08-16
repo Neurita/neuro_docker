c5e1bc87d563c44e33063130de6990331aba2ccb
Alexandre Manhaes Savio <alexsavio@gmail.com>
2016-08-16 18:16:41 +0200

    add dock_invoke.sh

---
+++ dock_invoke.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -x
+
+# A wrapper script for invoking a py-invoke tasks script
+# within a docker.
+
+VERSION="latest"
+
+# DATA BASE DIR
+# Note: this will be mounted in /data, make sure the file
+# path arguments reflext this.
+DATA_DIR=/home/alexandre/data
+
+# DOCKER_NAME
+DOCKER_NAME=dockerfile/neuro1
+
+# Helper functions for guards
+error(){
+  error_code=$1
+  echo "ERROR: $2" >&2
+  echo "($PROGNAME wrapper version: $VERSION, error code: $error_code )" &>2
+  exit $1
+}
+check_cmd_in_path(){
+  cmd=$1
+  which $cmd > /dev/null 2>&1 || error 1 "$cmd not found!"
+}
+
+# Guards (checks for dependencies)
+check_cmd_in_path docker
+
+# Set up mounted volumes, environment, and run our containerized command
+docker run \
+  --interactive --tty \
+  --volume "$DATA_DIR":/data \
+  --volume "$PWD":/wd \
+  --workdir /wd \
+  "$DOCKER_NAME:$VERSION" /bin/bash -c "$@"
